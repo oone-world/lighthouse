34a2a8b6fc0aeb8f0f5aafa8af9f388c866330d0
isaac.asimov <isaac.asimov@adigium.com>
2023-07-07 15:04:11 +0300

    Changed mainnet Base Reward Factor to 280

--- consensus/types/src/chain_spec.rs
+++ consensus/types/src/chain_spec.rs
@@ -540,7 +540,7 @@ impl ChainSpec {
             /*
              * Reward and penalty quotients
              */
-            base_reward_factor: 64,
+            base_reward_factor: 280,
             whistleblower_reward_quotient: 512,
             proposer_reward_quotient: 8,
             inactivity_penalty_quotient: u64::checked_pow(2, 26).expect("pow does not overflow"),
